6ad3f385830c208dd54d54d121a415a628f8ddcc
AliAkbarov99 <tu6lw8g66@code.edu.az>
2022-11-02 15:50:55 +0400

    Program is created

--- Credit calculator/Credit calculator/Program.cs
+++ Credit calculator/Credit calculator/Program.cs
@@ -2,13 +2,15 @@
 
 string username = "admin";
 string password = "admin";
-int price;
-int month;
-string percent;
-int sumprice;
-int monpayment;
-int operation;
-int agree;
+int price =0;
+int month = 0;
+string percent = " ";
+int monpayment = 0 ;
+int operation = 0;
+int agree = 0;
+int sumprice = 0;
+int remprice = 0;
+int remmonth = 0;
 tryagain:
 Console.WriteLine("Istifadeci adi daxil edin.");
 string logusername = Console.ReadLine();
@@ -31,7 +33,7 @@ if (username == logusername && password == logpassword)
         price = Convert.ToInt32(Console.ReadLine());
 
         sumprice = price + price / 100 * month;
-        
+        monpayment = sumprice / month;
         if(month <= 12)
         {
             Console.WriteLine($"Umumi mebleg:{sumprice}");
@@ -41,20 +43,46 @@ if (username == logusername && password == logpassword)
             Console.WriteLine($"Umumi mebleg:{sumprice}");
         }
 
-        Console.WriteLine($"Ayliq odenis:{sumprice / month}");
+        Console.WriteLine($"Ayliq odenis:{monpayment}");
 
         Console.WriteLine("Razisiniz? \n 1.Beli \n 2.Xeyir");
         agree = Convert.ToInt32(Console.ReadLine());
         if(agree == 1)
         {
             Console.WriteLine($"Emeliyyat ugurlu oldu! \n Indiki borcunuz: {sumprice} AZN");
+            remprice = sumprice;
+            remmonth = month;
         }
         goto backoperations;
 
     }
     else if(operation == 2)
     {
-        Console.WriteLine("2 desiz");
+        Console.WriteLine($"Ayliq odenis: {monpayment}");
+        paymentnum:
+        Console.WriteLine("Odemek ucun 1 basin");
+        int paymentnum =Convert.ToInt32(Console.ReadLine());
+        if (paymentnum == 1)
+        {
+            Console.WriteLine("Odenis ugurlu oldu !");
+            
+            remprice = remprice - monpayment;
+            remmonth = remmonth - 1;
+            Console.WriteLine($"Qalan borc:{remprice}\n Qalan ay:{remmonth}");
+            goto backoperations;
+        }
+        else
+        {
+            Console.WriteLine("Odenis olmadi.Yeniden daxil edin.");
+            goto paymentnum;
+        }
+    }
+
+    else if (operation == 3)
+    {
+        Console.WriteLine($"Qalan borc:{remprice}");
+        Console.WriteLine($"Qalan ay:{remmonth}");
+        goto backoperations;
     }
 
 }
