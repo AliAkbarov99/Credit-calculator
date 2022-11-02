74f6bf7a8d3380f781070a97ffdb29700610c09d
AliAkbarov99 <tu6lw8g66@code.edu.az>
2022-11-02 15:30:47 +0400

    operation 1 updated

--- Credit calculator/Credit calculator/Program.cs
+++ Credit calculator/Credit calculator/Program.cs
@@ -7,6 +7,8 @@ int month;
 string percent;
 int sumprice;
 int monpayment;
+int operation;
+int agree;
 tryagain:
 Console.WriteLine("Istifadeci adi daxil edin.");
 string logusername = Console.ReadLine();
@@ -15,7 +17,46 @@ string logpassword = Console.ReadLine();
 
 if (username == logusername && password == logpassword)
 {
+    backoperations:
     Console.WriteLine("\n \n 1.Kredit goturmek \n 2.Kredit odemek \n 3.Hesabat ");
+
+    Console.WriteLine("\nEmeliyyati secin.");
+    operation = Convert.ToInt32(Console.ReadLine());
+    if(operation == 1)
+    {
+        Console.WriteLine("Ay daxil edin");
+        month =Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine("Mebleg daxil edin");
+        price = Convert.ToInt32(Console.ReadLine());
+
+        sumprice = price + price / 100 * month;
+        
+        if(month <= 12)
+        {
+            Console.WriteLine($"Umumi mebleg:{sumprice}");
+        }
+        else
+        {
+            Console.WriteLine($"Umumi mebleg:{sumprice}");
+        }
+
+        Console.WriteLine($"Ayliq odenis:{sumprice / month}");
+
+        Console.WriteLine("Razisiniz? \n 1.Beli \n 2.Xeyir");
+        agree = Convert.ToInt32(Console.ReadLine());
+        if(agree == 1)
+        {
+            Console.WriteLine($"Emeliyyat ugurlu oldu! \n Indiki borcunuz: {sumprice} AZN");
+        }
+        goto backoperations;
+
+    }
+    else if(operation == 2)
+    {
+        Console.WriteLine("2 desiz");
+    }
+
 }
 else
 {
