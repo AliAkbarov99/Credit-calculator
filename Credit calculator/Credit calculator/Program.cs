f9170830c1b8526ef3a4d16df4ebe21cdb7a8403
AliAkbarov99 <tu6lw8g66@code.edu.az>
2022-11-02 14:32:23 +0400

    variables and login created

---
+++ Credit calculator/Credit calculator/Program.cs
@@ -0,0 +1,20 @@
+﻿
+
+string username = "admin";
+string password = "admin";
+
+tryagain:
+Console.WriteLine("Istifadeci adi daxil edin.");
+string logusername = Console.ReadLine();
+Console.WriteLine("Sifreni daxil edin.");
+string logpassword = Console.ReadLine();
+
+if (username == logusername && password == logpassword)
+{
+    Console.WriteLine("\n \n 1.Kredit goturmek \n 2.Kredit odemek \n 3.Hesabat ");
+}
+else
+{
+    Console.WriteLine("Istifadeci adi ve ya sifre yanlisdir. Xahis olunur yeniden yoxlayin.");
+    goto tryagain;
+}
